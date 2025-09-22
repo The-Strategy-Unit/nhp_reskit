a6792461e1eb02ed4e6602acea61abdf0999899f
Fran Barton <francis.barton@nhs.net>
2025-09-22 21:15:59 +0100

    📈 Tighten up code for change_factor_effects_plot

--- R/principal_detailed.R
+++ R/principal_detailed.R
@@ -37,109 +37,84 @@ principal_detailed_table <- function(data, aggregation, final_year) {
 
 
 principal_change_factor_effects_summarised <- function(
-  data,
+  dat,
   measure,
   include_baseline
 ) {
-  data <- data |>
+  bsline_filtered <- dplyr::filter(dat, .data[["change_factor"]] != "baseline")
+  dat_prepared <- if (include_baseline) dat else bsline_filtered
+
+  measure_data <- dat_prepared |>
     dplyr::filter(
-      .data$measure == .env$measure,
-      include_baseline | .data$change_factor != "baseline",
-      .data$value != 0
+      dplyr::if_any("measure", \(x) x == {{ measure }}) &
+        dplyr::if_any("value", \(x) x != 0)
     ) |>
-    tidyr::drop_na("value") |>
+    dplyr::summarise(dplyr::across("value", sum), .by = "change_factor") |>
     dplyr::mutate(
-      dplyr::across(
-        "change_factor",
-        \(.x) forcats::fct_reorder(.x, -.data$value)
-      ),
+      dplyr::across("change_factor", \(x) {
+        forcats::fct_reorder(x, .data[["value"]], .desc = TRUE)
+      })
+    )
+
+  bl_first <- measure_data |>
+    dplyr::mutate(
       # baseline may now not be the first item, move it back to start
-      dplyr::across(
-        "change_factor",
-        \(.x) forcats::fct_relevel(.x, "baseline")
-      )
+      dplyr::across("change_factor", \(x) forcats::fct_relevel(x, "baseline"))
     )
+  measure_data <- if (include_baseline) bl_first else measure_data
 
-  cfs <- data |>
-    dplyr::group_by(.data$change_factor) |>
-    dplyr::summarise(dplyr::across("value", \(.x) sum(.x, na.rm = TRUE))) |>
-    dplyr::mutate(cuvalue = cumsum(.data$value)) |>
+  estimate_row <- tibble::tibble_row(
+    change_factor = "Estimate",
+    hidden = 0,
+    total = sum(measure_data[["value"]])
+  )
+
+  measure_data |>
     dplyr::mutate(
-      hidden = tidyr::replace_na(
-        dplyr::lag(.data$cuvalue) + pmin(.data$value, 0),
-        0
-      ),
-      colour = dplyr::case_when(
-        .data$change_factor == "Baseline" ~ "#686f73",
-        .data$value >= 0 ~ "#f9bf07",
-        TRUE ~ "#2c2825"
-      ),
-      dplyr::across("value", abs)
+      cuvalue = cumsum(.data[["value"]]),
+      hidden = dplyr::lag(.data[["cuvalue"]], 1, 0) + pmin(.data[["value"]], 0)
     ) |>
-    dplyr::select(-"cuvalue")
-
-  levels <- unique(c(
-    "baseline",
-    levels(forcats::fct_drop(cfs$change_factor)),
-    "Estimate"
-  ))
-  if (!include_baseline) {
-    levels <- levels[-1]
-  }
-
-  cfs |>
-    dplyr::bind_rows(
-      dplyr::tibble(
-        change_factor = "Estimate",
-        value = sum(data$value),
-        hidden = 0,
-        colour = "#ec6555"
-      )
-    ) |>
-    tidyr::pivot_longer(c("value", "hidden")) |>
-    dplyr::mutate(
-      dplyr::across("colour", \(.x) ifelse(.data$name == "hidden", NA, .x)),
-      dplyr::across("name", \(.x) forcats::fct_relevel(.x, "hidden", "value")),
-      dplyr::across("change_factor", \(.x) factor(.x, rev(levels)))
-    )
+    dplyr::select(c("change_factor", "hidden", total = "value")) |>
+    dplyr::bind_rows(estimate_row)
+  # tidyr::pivot_longer(c("value", "hidden"))
 }
 
 
-principal_change_factor_effects_cf_plot <- function(data) {
-  # Reorient data for geom_segment
-  data_reoriented <- data |>
-    tidyr::pivot_wider(
-      id_cols = tidyselect::all_of("change_factor"),
-      names_from = tidyselect::all_of("name"),
-      values_from = tidyselect::all_of("value")
-    ) |>
-    dplyr::mutate(colour = data[["colour"]][!is.na(data[["colour"]])])
-
-  data_reoriented |>
+principal_change_factor_effects_plot <- function(pcfe_data) {
+  pcfe_data |>
     dplyr::mutate(
-      xstart = .data[["hidden"]],
-      xend = .data[["hidden"]] + .data[["value"]]
+      colour = dplyr::case_when(
+        .data[["change_factor"]] == "baseline" ~ "#686f73",
+        .data[["change_factor"]] == "Estimate" ~ "#ec6555",
+        .data[["value"]] >= 0 ~ "#f9bf07",
+        .default = "#2c2825"
+      ),
+      dplyr::across("value", abs), # ?
+      # dplyr::across("name", \(x) forcats::fct(x, c("hidden", "value"))),
+      dplyr::across("change_factor", forcats::fct_rev),
+      dplyr::across("change_factor", \(x) forcats::fct_relevel(x, "Estimate")),
+      dplyr::across("total", \(x) x + .data[["hidden"]])
     ) |>
     ggplot2::ggplot(
       ggplot2::aes(
-        x = .data[["xstart"]],
-        xend = .data[["xend"]],
+        x = .data[["hidden"]],
+        xend = .data[["total"]],
         y = .data[["change_factor"]],
-        yend = .data[["change_factor"]], # plotly errors if yend not included
+        yend = .data[["change_factor"]],
         colour = .data[["colour"]]
       )
     ) +
     ggplot2::geom_segment(
       # dynamic: bigger if fewer bars (130 is relative to 600px plot height)
-      lwd = 130 / nrow(data_reoriented)
+      lwd = 130 / nrow(pcfe_data)
     ) +
     ggplot2::scale_colour_identity() +
     ggplot2::scale_x_continuous(
       breaks = scales::pretty_breaks(5),
-      labels = scales::comma
+      labels = scales::label_comma()
     ) +
     ggplot2::scale_y_discrete(labels = snakecase::to_title_case) +
-    ggplot2::labs(x = "", y = "")
+    ggplot2::labs(x = NULL, y = NULL)
 }
 
 
