f63cbef35dc611360667639f64538f211ca82632
Fran Barton <francis.barton@nhs.net>
2025-11-18 17:43:38 +0000

    🏥 Implement summarise_to_selected_sites

--- R/misc.R
+++ R/misc.R
@@ -1,25 +1,19 @@
-summarise_to_site <- function(dat, site, sum_by, site_col = "sitetret") {
+summarise_to_selected_sites <- function(dat, sites, site_col = "sitetret") {
+  dat <- if (is.null(sites)) {
+    dat
+  } else {
+    dplyr::filter(dat, dplyr::if_any({{ site_col }}, \(x) x %in% {{ sites }}))
+  }
   dat |>
-    filter_to_site(sites, site_col) |>
+    dplyr::select(!tidyselect::all_of({{ site_col }})) |>
     dplyr::summarise(
-      # was: across(where(is.numeric)) - trying being more specific here
-      dplyr::across(c("baseline", "principal"), sum),
-      .by = tidyselect::all_of(sum_by)
+      dplyr::across(tidyselect::where(is.numeric), sum),
+      .by = tidyselect::where(\(x) !is.numeric(x))
     )
 }
 
-
-filter_to_site <- function(dat, site, site_col = "sitetret") {
-  dat <- if (is.null(site)) {
-    dat
-  } else {
-    dplyr::filter(dat, dplyr::if_any({{ site_col }}, \(x) x == {{ site }}))
-  }
-  dplyr::select(dat, !tidyselect::all_of({{ site_col }}))
-}
-
-
 get_trust_sites <- \(res_tbl, col = "sitetret") sort(unique(res_tbl[[col]]))
 
+convert_sex_codes <- \(x) dplyr::if_else(x == 1L, "Male", "Female")
 
 uppercase_init <- \(x) sub("^([a-z])(.+)", "\\U\\1\\E\\2", x, perl = TRUE)
--- R/principal_summary_data.R
+++ R/principal_summary_data.R
@@ -1,4 +1,4 @@
-compile_principal_summary_data <- function(results) {
+compile_principal_summary_data <- function(results, sites = NULL) {
   main_measures <- c("admissions", "attendances", "walk-in", "ambulance")
   main_summary <- results |>
     get_principal_high_level_summary(main_measures)
@@ -23,26 +23,24 @@ compile_principal_summary_data <- function(results) {
         forcats::fct(x, levels = c("Inpatient", "Outpatient", "A&E"))
       })
     ) |>
+    summarise_to_selected_sites(sites) |>
     add_change_cols() |>
     dplyr::arrange(dplyr::pick(c("activity_type_label", "pod_name")))
 }
 
 
 # "tretspef+los_group" table after calculate_principal() has been applied
-compile_principal_summary_los_data <- function(los_tbl, site, this_measure) {
+compile_principal_summary_los_data <- function(los_tbl, sites = NULL, measure) {
   summary_los_data <- los_tbl |>
-    dplyr::filter(dplyr::if_any("measure", \(x) x == {{ this_measure }})) |>
-    dplyr::summarise(
-      dplyr::across(c("baseline", "principal"), sum),
-      .by = c("pod", "sitetret", "los_group") # also tretspef?
-    ) |>
+    dplyr::filter(dplyr::if_any("measure", \(x) x == {{ measure }})) |>
     dplyr::inner_join(get_principal_pods(), "pod") |>
     dplyr::relocate("pod_name") |>
     dplyr::select(!c("pod", "activity_type", "measure")) |>
-    dplyr::distinct() |>
+    summarise_to_selected_sites(sites) |>
+    # dplyr::distinct() |>
     add_change_cols()
 
-  if (this_measure == "beddays") {
+  if (measure == "beddays") {
     # can also be "admissions"
     summary_los_data <- summary_los_data |>
       dplyr::mutate(
