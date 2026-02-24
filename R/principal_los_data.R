a6cb26c52d9261a6a556ba7937de2e4ad28dd27d
Fran Barton <francis.barton@nhs.net>
2026-02-24 12:39:50 +0000

    📛 Rename things
    📛 Rename things
    
    📛 Rename things
    
    📛 Rename things
    
    📛 Rename things
    
    📛 Rename things

--- R/principal_los_data.R
+++ R/principal_los_data.R
@@ -8,7 +8,7 @@
 #'  by point of delivery and grouped length of stay
 #' @export
 compile_principal_los_data <- function(los_tbl, measure, sites = NULL) {
-  summary_los_data <- prepare_sites_principal_los_data(los_tbl, measure) |>
+  summary_los_data <- prepare_principal_los_data(los_tbl, measure) |>
     filter_to_selected_sites(sites) |>
     summarise_for_all_sites() |>
     add_change_cols()
@@ -35,7 +35,7 @@ compile_principal_los_data <- function(los_tbl, measure, sites = NULL) {
 #' @inheritParams compile_principal_los_data
 #' @returns A tibble
 #' @keywords internal
-prepare_sites_principal_los_data <- function(los_tbl, selected_measure) {
+prepare_principal_los_data <- function(los_tbl, selected_measure) {
   los_tbl |>
     dplyr::filter(
       dplyr::if_any("measure", \(x) x == .env[["selected_measure"]])
@@ -54,8 +54,8 @@ prepare_sites_principal_los_data <- function(los_tbl, selected_measure) {
 #' @inheritParams compile_principal_los_data
 #' @returns A tibble
 #' @export
-export_sites_principal_los_data <- function(los_tbl, sites = NULL) {
-  prepare_sites_principal_los_data(los_tbl) |>
+export_principal_los_data <- function(los_tbl, sites = NULL) {
+  prepare_principal_los_data(los_tbl) |>
     filter_to_selected_sites(sites) |>
     add_change_cols() |>
     dplyr::arrange(dplyr::pick(c("activity_type_label", "pod_label")))
