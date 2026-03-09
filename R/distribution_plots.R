3ed4287400a0981d611106ed421f6ef5fd78ce87
Fran Barton <francis.barton@nhs.net>
2026-03-09 14:09:58 +0000

    :nail_care: Amend presentation of beeswarm and ecdf plots Should close #81

--- R/distribution_plots.R
+++ R/distribution_plots.R
@@ -11,15 +11,21 @@ make_beeswarm_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
   stopifnot(all(lengths(c(baseline_value, principal_value)) == 1))
   min_value <- min(distrib_plot_data[["value"]])
   min_x_value <- ifelse(show_zero, 0, min(baseline_value, min_value))
-  plot_red <- scales::pal_hue()(1)
-  distrib_plot_data |>
+  plot_red <- "red"
+  interim_plot <- distrib_plot_data |>
     ggplot2::ggplot(ggplot2::aes(.data[["value"]], 1)) +
     ggbeeswarm::geom_quasirandom(
+      ggplot2::aes(
+        text = glue::glue("Value: {scales::comma(value, accuracy = 1)}")
+      ),
       colour = plot_red,
       orientation = "y",
       size = 3,
-      alpha = 0.6
-    ) +
+      alpha = 0.5
+    ) |>
+      # `text` aesthetic causes warnings when not used with plotly
+      suppressWarnings()
+  interim_plot +
     ggplot2::geom_vline(
       xintercept = baseline_value,
       colour = "grey50",
@@ -34,9 +40,10 @@ make_beeswarm_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
     ggplot2::expand_limits(x = ifelse(show_zero, 0, min_x_value)) +
     ggplot2::scale_x_continuous(
       breaks = scales::breaks_pretty(8),
-      labels = scales::label_number(scale_cut = scales::cut_short_scale()),
+      labels = scales::label_comma(),
       expand = ggplot2::expansion(0.01)
     ) +
+    ggplot2::scale_y_continuous(expand = ggplot2::expansion(0.15)) +
     ggplot2::theme(
       axis.title.x = ggplot2::element_blank(),
       axis.ticks.y = ggplot2::element_blank(),
@@ -64,8 +71,8 @@ make_cumulative_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
   principal_match_value <- which.min(principal_diffs)
   principal_pct <- y_vals[[principal_match_value]]
   min_x_value <- ifelse(show_zero, 0, min(baseline_value, min_value))
-  plot_red <- scales::pal_hue()(1)
-  plot_blue <- scales::pal_hue()(3)[[3]]
+  plot_red <- "red"
+  plot_blue <- "cornflowerblue"
 
   line_guides <- tibble::tibble(
     x_start = c(rep(min_x_value, 3), x_quantiles, principal_value),
@@ -74,8 +81,22 @@ make_cumulative_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
     y_end = rep(c(quantiles, principal_pct), 2),
     colour = rep(c(rep(plot_red, 2), plot_blue), 2)
   )
-  tibble::tibble(x = x_vals, y = y_vals) |>
+  interim_plot <- tibble::tibble(x = x_vals, y = y_vals) |>
     ggplot2::ggplot(ggplot2::aes(.data[["x"]], .data[["y"]])) +
+    # points not really visible, but necessary for plotly tooltips to be used
+    ggplot2::geom_point(
+      ggplot2::aes(
+        text = glue::glue(
+          "Percentage: {scales::percent(y_vals, accuracy = 1)}\n",
+          "Value: {scales::comma(x_vals, accuracy = 1)}"
+        )
+      ),
+      alpha = 0.01,
+      size = 0.01
+    ) |>
+      # `text` aesthetic causes warnings when not used with plotly
+      suppressWarnings()
+  interim_plot +
     ggplot2::geom_step(colour = "grey50", linewidth = 1.2) +
     ggplot2::geom_segment(
       data = line_guides,
@@ -83,9 +104,11 @@ make_cumulative_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
         x = .data[["x_start"]],
         y = .data[["y_start"]],
         xend = .data[["x_end"]],
-        yend = .data[["y_end"]],
-        colour = .data[["colour"]]
+        yend = .data[["y_end"]]
       ),
+      # colour removed as an explicit aesthetic because plotly doesn't respect
+      # `show.legend = FALSE`
+      colour = line_guides[["colour"]],
       linetype = "dashed",
       linewidth = 1.2,
       show.legend = FALSE
@@ -98,7 +121,7 @@ make_cumulative_distrib_plot <- function(distrib_plot_data, show_zero = FALSE) {
     ggplot2::expand_limits(x = min_x_value) +
     ggplot2::scale_x_continuous(
       breaks = scales::pretty_breaks(8),
-      labels = scales::label_number(scale_cut = scales::cut_short_scale()),
+      labels = scales::label_comma(),
       expand = ggplot2::expansion(0.01)
     ) +
     ggplot2::scale_y_continuous(
