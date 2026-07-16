7d456dbdc5695ae05dd27e7b7bb815de394befb3
Fran Barton <francis.barton@nhs.net>
2026-07-16 19:19:17 +0100

    :otter: Refactor functions that pull lookup files from GitHub Should close #153

--- R/get_github_file.R
+++ R/get_github_file.R
@@ -1,15 +1,16 @@
-#' Read in a file from the NHP Outputs app GitHub repo
+#' Read in a file from a Strategy Unit GitHub repo
 #'
+#' @param repo string. The name of the repository in which to find the file
+#' @param folder string. The folder where the file is located. Set to `""` to
+#'   use the root folder of the repo.
 #' @param file string. The name of the file to read in
-#' @param folder string. The folder where the file is located. `"inst"` by
-#'   default. Set to `""` to use the root folder of the repo.
 #' @returns The file contents as a stream, to be passed to a reader function
 #' @keywords internal
-get_outputs_gh_file <- function(file, folder = "inst") {
+get_su_gh_file <- function(repo, folder, file) {
   httr2::request("https://api.github.com") |>
     httr2::req_url_path_append("repos") |>
     httr2::req_url_path_append("The-Strategy-Unit") |>
-    httr2::req_url_path_append("nhp_outputs") |>
+    httr2::req_url_path_append(repo) |>
     httr2::req_url_path_append("contents") |>
     httr2::req_url_path_append(folder) |>
     httr2::req_url_path_append(file) |>
@@ -18,31 +19,25 @@ get_outputs_gh_file <- function(file, folder = "inst") {
     httr2::resp_body_json() |>
     purrr::pluck("content") |>
     base64enc::base64decode()
+}
+
+
+#' Read in a file from the NHP Outputs app GitHub repo
+#'
+#' @param ... Pass the name of the file in via `...`
+#' @keywords internal
+get_outputs_gh_file <- function(...) {
+  purrr::partial(get_su_gh_file, repo = "nhp_outputs", folder = "inst")(...)
 }
 
 
 #' Read in a file from the TPMAs GitHub repo
 #'
-#' @param file string. The name of the file to read in
-#' @param folder string. The folder where the file is located. `"reference"` by
-#'   default. Set to `""` to use the root folder of the repo.
-#' @returns The file contents as a stream, to be passed to a reader function
+#' @inheritParams get_outputs_gh_file
 #' @keywords internal
-get_tpmas_gh_file <- function(file, folder = "reference") {
-  httr2::request("https://api.github.com") |>
-    httr2::req_url_path_append("repos") |>
-    httr2::req_url_path_append("The-Strategy-Unit") |>
-    httr2::req_url_path_append("TPMAs") |>
-    httr2::req_url_path_append("contents") |>
-    httr2::req_url_path_append(folder) |>
-    httr2::req_url_path_append(file) |>
-    httr2::req_perform() |>
-    httr2::resp_check_status() |>
-    httr2::resp_body_json() |>
-    purrr::pluck("content") |>
-    base64enc::base64decode()
+get_tpmas_gh_file <- function(...) {
+  purrr::partial(get_su_gh_file, repo = "TPMAs", folder = "reference")(...)
 }
-
 
 possibly_get_outputs_gh_file <- \(...) purrr::possibly(get_outputs_gh_file)(...)
 possibly_get_tpmas_gh_file <- \(...) purrr::possibly(get_tpmas_gh_file)(...)
