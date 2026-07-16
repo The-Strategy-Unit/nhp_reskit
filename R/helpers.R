5b8609df46c1f323944c1f194b9cb85a46bf370d
Fran Barton <francis.barton@nhs.net>
2026-07-16 19:26:05 +0100

    :fire: Remove local copies of lookup for pods and tpmas Should close #146

--- R/helpers.R
+++ R/helpers.R
@@ -182,9 +182,10 @@ get_tretspef_lookup <- function() {
 #'  `change_factor` and `tpma_label`
 #' @export
 get_tpma_label_lookup <- function() {
-  csv_data <- possibly_read_tpmas_lookup()
+  csv_data_raw <- possibly_get_tpmas_gh_file("tpma-lookup.csv")
   msg <- "Unable to read TPMA lookup table from GitHub"
-  azkit::check_that(csv_data, is_not_null, msg)
+  azkit::check_that(csv_data_raw, is_not_null, msg)
+  csv_data <- readr::read_csv(csv_data_raw, col_types = "-ccccc---c")
   csv_data |>
     dplyr::filter(dplyr::if_any("active_to", is.na)) |>
     dplyr::select(!"active_to") |>
