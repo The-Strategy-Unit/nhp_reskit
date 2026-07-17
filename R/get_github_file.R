f2b934ac0a126520b25a7431206b5109acf52c1f
Fran Barton <francis.barton@nhs.net>
2026-07-17 16:26:03 +0100

    :yo_yo: Handle lookups from GitHub via direct URLs Should close #162

--- R/get_github_file.R
+++ R/get_github_file.R
@@ -1,48 +1,42 @@
-#' Read in a file from the NHP Outputs app GitHub repo
+#' Get the direct URL to a file in the NHP Outputs GitHub repo
 #'
-#' @param file string. The name of the file to read in
-#' @param folder string. The folder where the file is located. `"inst"` by
-#'   default. Set to `""` to use the root folder of the repo.
-#' @returns The file contents as a stream, to be passed to a reader function
+#' @param ... Pass the name of the file in via `...`
 #' @keywords internal
-get_outputs_gh_file <- function(file, folder = "inst") {
-  httr2::request("https://api.github.com") |>
-    httr2::req_url_path_append("repos") |>
-    httr2::req_url_path_append("The-Strategy-Unit") |>
-    httr2::req_url_path_append("nhp_outputs") |>
-    httr2::req_url_path_append("contents") |>
-    httr2::req_url_path_append(folder) |>
-    httr2::req_url_path_append(file) |>
-    httr2::req_perform() |>
-    httr2::resp_check_status() |>
-    httr2::resp_body_json() |>
-    purrr::pluck("content") |>
-    base64enc::base64decode()
+get_outputs_gh_url <- function(...) {
+  purrr::partial(get_su_gh_file, repo = "nhp_outputs", folder = "inst")(...)
 }
 
-
-#' Read in a file from the TPMAs GitHub repo
+#' Get the direct URL to a file in the TPMAs GitHub repo
 #'
-#' @param file string. The name of the file to read in
-#' @param folder string. The folder where the file is located. `"reference"` by
-#'   default. Set to `""` to use the root folder of the repo.
-#' @returns The file contents as a stream, to be passed to a reader function
+#' @inheritParams get_outputs_gh_url
 #' @keywords internal
-get_tpmas_gh_file <- function(file, folder = "reference") {
-  httr2::request("https://api.github.com") |>
-    httr2::req_url_path_append("repos") |>
-    httr2::req_url_path_append("The-Strategy-Unit") |>
-    httr2::req_url_path_append("TPMAs") |>
-    httr2::req_url_path_append("contents") |>
-    httr2::req_url_path_append(folder) |>
-    httr2::req_url_path_append(file) |>
-    httr2::req_perform() |>
-    httr2::resp_check_status() |>
-    httr2::resp_body_json() |>
-    purrr::pluck("content") |>
-    base64enc::base64decode()
+get_tpmas_gh_url <- function(...) {
+  purrr::partial(get_su_gh_file, repo = "TPMAs", folder = "reference")(...)
 }
 
+#' Read the pods lookup table from a YAML file in the NHP Outputs repo
+read_pods_lookup <- function(file = "golem-config.yml") {
+  yaml12::parse_yaml(readr::read_lines(get_outputs_gh_url(file)))
+}
+possibly_read_pods_lookup <- \(...) purrr::possibly(read_pods_lookup)(...)
 
-possibly_get_outputs_gh_file <- \(...) purrr::possibly(get_outputs_gh_file)(...)
-possibly_get_tpmas_gh_file <- \(...) purrr::possibly(get_tpmas_gh_file)(...)
+#' Read the TPMAs lookup table from a CSV file in the TPMAs repo
+read_tpmas_lookup <- function(file = "tpma-lookup.csv") {
+  readr::read_csv(get_tpmas_gh_url(file), col_types = "-ccccc---c")
+}
+possibly_read_tpmas_lookup <- \(...) purrr::possibly(read_tpmas_lookup)(...)
+
+
+#' Read in a file from a Strategy Unit GitHub repo
+#'
+#' @param repo string. The name of the repository in which to find the file
+#' @param folder string. The folder where the file is located. Set to `""` to
+#'   use the root folder of the repo.
+#' @param file string. The name of the file to read in
+#' @returns The URL to the raw file contents, to be passed to a reader function
+#' @keywords internal
+get_su_gh_file <- function(repo, folder, file) {
+  raw_base_url <- "https://raw.githubusercontent.com"
+  su <- "The-Strategy-Unit"
+  file.path(raw_base_url, su, repo, "refs", "heads", "main", folder, file)
+}
